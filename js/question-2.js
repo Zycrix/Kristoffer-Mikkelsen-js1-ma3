577459df4d17afcc76bf71ddd4d729dcb34c4be4
Zycrix <Doffa98@live.no>
2022-02-11 21:20:44 +0100

    work on the assignment

--- js/question-2.js
+++ js/question-2.js
@@ -1,45 +1,51 @@
 // Question 2
 
-const apiURL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b9e5599f2eee47c28d9200d087a81d21"
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&page=1&key=b9e5599f2eee47c28d9200d087a81d21";
+const loader = document.querySelector(".loadingContainer");
+const content = document.querySelector(".content");
+let toBeDisplayed = 8;
+let tagLength;
 
 
+//Remove loading indicator
+function stopLoading() {
 
-
-function createHTML(results) {
-  const facts = results;
-
-
-  console.log(facts[2].name);
- /*  resultsContainer.innerHTML = "";
-
-      for (let i = 0; i < facts.length; i++) {
-
-          if (i === 10) {
-              break;
-          }
-
-          resultsContainer.innerHTML += `<div class="result">${facts[i].text}</div>`;
-      } */
+  loader.style.display = "none";
+  
 }
 
+
+//Create html from the api results
+function htmlCreator(data){
+
+  stopLoading();
+
+  for (let i = 0; i < toBeDisplayed; i++){
+
+    tagLength = data[i].tags.length;
+
+
+    content.innerHTML += `<div class = "newContentContainer">
+      <h2 class = "newContentName">${data[i].name}</h2>
+      <h3 class = "newContentRating">Rating: ${data[i].rating}</h3>
+      <h4 class = "newContentTags">Number of tags: ${tagLength}</h4>
+      </div>`
+  }
+}
+
+
+//API call
 async function callAPI(){
 
-    const response = await fetch(apiURL);
+    const response = await fetch(url);
     const result = await response.json();
-    const data = result.all;
-
-    createHTML(data);
-    console.log(response);
-
-
-  /*   for (let i = 0; i < 10; i++){
-
-      console.log(data[i].text)
-
-      if(i === 7){
-        break;
-      }
-    } */
+    const data = result.results;
+    
+    htmlCreator(data);
 }
 
-callAPI();
+
+callAPI(); 
+
+
+//https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b9e5599f2eee47c28d9200d087a81d21
