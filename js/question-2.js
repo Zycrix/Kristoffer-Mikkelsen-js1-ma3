1a55627e31964b17c1f60b4c1bab09a9adbb6863
Zycrix <Doffa98@live.no>
2022-02-12 19:29:54 +0100

    finishing touches

--- js/question-2.js
+++ js/question-2.js
@@ -12,40 +12,51 @@ function stopLoading() {
 
   loader.style.display = "none";
   
-}
+};
 
 
 //Create html from the api results
 function htmlCreator(data){
 
-  stopLoading();
-
   for (let i = 0; i < toBeDisplayed; i++){
 
     tagLength = data[i].tags.length;
-
 
     content.innerHTML += `<div class = "newContentContainer">
       <h2 class = "newContentName">${data[i].name}</h2>
       <h3 class = "newContentRating">Rating: ${data[i].rating}</h3>
       <h4 class = "newContentTags">Number of tags: ${tagLength}</h4>
-      </div>`
-  }
-}
+      </div>`;
+  };
+};
 
 
 //API call
 async function callAPI(){
 
+  try{
+
     const response = await fetch(url);
     const result = await response.json();
     const data = result.results;
-    
-    htmlCreator(data);
-}
 
+    stopLoading();
+    htmlCreator(data);
+
+  }
+  catch(error){
+
+    stopLoading();
+
+    content.classList.add("error");
+    content.innerHTML = `<p>An error has occured please try again</p>
+    <button class="errorButton">Try again</button>`;
+
+    const errorButton = document.querySelector(".errorButton");
+
+    errorButton.onclick = () => location.reload();
+
+  };
+};
 
 callAPI(); 
-
-
-//https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b9e5599f2eee47c28d9200d087a81d21
